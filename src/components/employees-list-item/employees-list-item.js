7d64e5ca9d64245f2c058b8775d0ce2214e5e3c6
Nick <xa12850328@student.karazin.ua>
2022-05-30 19:20:46 +0300

    Add some code and functionality

---
+++ src/components/employees-list-item/employees-list-item.js
@@ -0,0 +1,30 @@
+import './employees-list-item.css';
+
+const EmployeesListItem = ({name, salary, increase}) => {
+
+    let classNames = "list-group-item d-flex justify-content-between";
+    if (increase) {
+        classNames += " increase";
+    }
+
+    return (
+        <li className={classNames}>
+            <span className="list-group-item-label">{name}</span>
+            <input type="text" className="list-group-item-input" defaultValue={salary + '$'}/>
+            <div className='d-flex justify-content-center align-items-center'>
+                <button type="button"
+                    className="btn-cookie btn-sm ">
+                    <i className="fas fa-cookie"/>
+                </button>
+
+                <button type="button"
+                        className="btn-trash btn-sm ">
+                    <i className="fas fa-trash"/>
+                </button>
+                <i className="fas fa-star"/>
+            </div>
+        </li>
+    )
+}
+
+export default EmployeesListItem;
